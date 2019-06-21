0ea8c91aa6d1ddae3f4115e831254d30880f689a
陈真 <chenzhen.a@yuewen.com>
2019-06-21 18:54:23 +0800

    Mybatis接入项目，配置Mybatis.xml的自动生成器

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -3,6 +3,7 @@ package com.miaoshaproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,7 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App
 {
-    
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         SpringApplication.run(App.class,args);
